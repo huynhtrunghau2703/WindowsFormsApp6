ae4f7a715c382820dc287302cdfde5ffcf9d505a
ACER <ACER@LAPTOP-QCLO3M1N>
2022-11-24 18:50:39 +0700

    Add project files.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculator = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.txtHeSoA = new System.Windows.Forms.TextBox();
+            this.txtHeSoB = new System.Windows.Forms.TextBox();
+            this.txtHeSoC = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculator
+            // 
+            this.btnCalculator.Location = new System.Drawing.Point(186, 278);
+            this.btnCalculator.Name = "btnCalculator";
+            this.btnCalculator.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculator.TabIndex = 0;
+            this.btnCalculator.Text = "BtnCalculator";
+            this.btnCalculator.UseVisualStyleBackColor = true;
+            this.btnCalculator.Click += new System.EventHandler(this.BtnCalculator_Click);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(406, 278);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancel.TabIndex = 1;
+            this.BtnCancel.Text = "button2";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // txtHeSoA
+            // 
+            this.txtHeSoA.Location = new System.Drawing.Point(170, 39);
+            this.txtHeSoA.Name = "txtHeSoA";
+            this.txtHeSoA.Size = new System.Drawing.Size(100, 22);
+            this.txtHeSoA.TabIndex = 2;
+            // 
+            // txtHeSoB
+            // 
+            this.txtHeSoB.Location = new System.Drawing.Point(186, 106);
+            this.txtHeSoB.Name = "txtHeSoB";
+            this.txtHeSoB.Size = new System.Drawing.Size(100, 22);
+            this.txtHeSoB.TabIndex = 3;
+            this.txtHeSoB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtHeSo_KeyPress);
+            // 
+            // txtHeSoC
+            // 
+            this.txtHeSoC.Location = new System.Drawing.Point(200, 167);
+            this.txtHeSoC.Name = "txtHeSoC";
+            this.txtHeSoC.Size = new System.Drawing.Size(100, 22);
+            this.txtHeSoC.TabIndex = 4;
+            this.txtHeSoC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtHeSo_KeyPress);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(209, 217);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 22);
+            this.txtResult.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "C";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(101, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "reult";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtHeSoC);
+            this.Controls.Add(this.txtHeSoB);
+            this.Controls.Add(this.txtHeSoA);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.btnCalculator);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtHeSo_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculator;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.TextBox txtHeSoA;
+        private System.Windows.Forms.TextBox txtHeSoB;
+        private System.Windows.Forms.TextBox txtHeSoC;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtHeSoA.KeyPress += TxtHeSo_KeyPress;
+            txtHeSoB.KeyPress += TxtHeSo_KeyPress;
+            txtHeSoC.KeyPress += TxtHeSo_KeyPress;
+            btnCalculator.Click += BtnCalculator_Click;
+            BtnCancel.Click += BtnCancel_Click;
+            txtResult.Enabled = false;
+            txtResult.ForeColor = Color.Aquamarine;
+        }
+
+        private void TxtHeSo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar)) // không phải là kí tự
+                e.Handled = true;
+        }
+
+  
+
+        private void BtnCalculator_Click(object sender, EventArgs e)
+        {
+            int hesoa = int.Parse(txtHeSoA.Text);
+            int hesob = int.Parse(txtHeSoB.Text);
+            int hesoc = int.Parse(txtHeSoC.Text);
+            // Kiểm tra các hệ số do người dùng nhập
+            if (hesoa == 0)
+            {
+                if (hesob == 0)
+                {
+                    if (hesoc == 0)
+                        txtResult.Text = "Equation has infinite soluations";
+                    else
+                        txtResult.Text = "Equation has not solution";
+                }
+                else
+                {
+                    double result = -hesoc / (hesob * 1.0);
+                    result = Math.Round(result, 2);
+                    txtResult.Text = "x1 = x2 = " + result.ToString();
+                }
+            }
+            else
+            {
+                double delta = hesob * hesob - 4 * hesoa * hesoc;
+                if (delta == 0)
+                {
+                    double result = -hesob / (2 * hesoa * 1.0);
+                    result = Math.Round(result, 2);
+                    txtResult.Text = "x1 = x2 = " + result.ToString();
+                }
+                else if (delta < 0)
+                    txtResult.Text = "Equation has not solution";
+                else
+                {
+                    double x1 = (-hesob + Math.Sqrt(delta)) / (2 * hesoa * 1.0);
+                    double x2 = (-hesob - Math.Sqrt(delta)) / (2 * hesoa * 1.0);
+                    x1 = Math.Round(x1, 2);
+                    x2 = Math.Round(x2, 2);
+                    txtResult.Text = "x1 = " + x1.ToString() + " and " + "x2 = " + x2.ToString();
+                }
+            }
+
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            btnCalculator.Click -= BtnCalculator_Click;
+        }
+    }
+
+    
+}
+    
